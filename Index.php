5ab0649e1a8e05c7b73c12a16625fd876a2292b9
Noe Cortez <corteznoe3@gmail.com>
2021-08-24 00:50:07 -0600

    Registro UI terminado

--- Index.php
+++ Index.php
@@ -8,33 +8,56 @@
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' media='screen' href='styles/style.css'>
     <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <script src='main.js'></script>
 </head>
 
 <body>
-    <div class="container">
-        <h1>HOLA WORLD !!!</h1>
-        <h3>Hello people</h3>
-        <p>Genial!!! Estamos con todos los poderes. Me llega 😎</p>
-    </div>
 
+    <!-- SIGN UP -->
+    <div class="signup">
+        <h1>Profile Register</h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eu diam vitae urna imperdiet maximus sed eu nibh. Donec ac dapibus metus. Sed eget massa et purus maximus sodales id eu purus.
+        </p>
+        <div class="controls">
+            <input
+                type="text"
+                name="username"
+                id="username"
+                placeholder="Username"
+                require
+            >
 
-    <!-- Agregando tarjeta -->
-    <div class="card" style="width: 18rem;">
-        <div class="card-body">
-            <h5 class="card-title">Card "First"</h5>
-            <h6 class="card-subtitle mb-2 text-muted">Card</h6>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                contento.</p>
-            <a href="#" class="card-link">Card </a>
-            <a href="#" class="card-link">Another enlace</a>
+            <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Password"
+                require
+            >
+
+            <input
+                type="text"
+                name="classname"
+                id="classname"
+                placeholder="Class Name"
+                require
+            >
+
+            <input
+                type="number"
+                name="level"
+                id="level"
+                placeholder="Level"
+                require
+            >
+
+            <input type="button" value="sign up" id="button-sign">
+
         </div>
     </div>
 
 </body>
 
-<!-- LOGIN -->
-<!-- SIGN UP -->
 </html>
