f70da807c54713877ee00d268488704e0865e1a9
Noe Cortez <corteznoe3@gmail.com>
2021-08-23 23:00:01 -0600

    Creacion de directorios y cambio de extension a PHP

--- Index.php
+++ Index.php
@@ -6,7 +6,7 @@
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>Page Title</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='style.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='styles/style.css'>
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
@@ -19,18 +19,10 @@
         <h3>Hello people</h3>
         <p>Genial!!! Estamos con todos los poderes. Me llega 😎</p>
     </div>
-
-    <!-- Agregando tarjeta -->
-    <div class="card" style="width: 18rem;">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                content.</p>
-            <a href="#" class="card-link">Card link</a>
-            <a href="#" class="card-link">Another link</a>
-        </div>
-    </div>
 </body>
+
+<?php
+    phpinfo();
+?>
 
 </html>
