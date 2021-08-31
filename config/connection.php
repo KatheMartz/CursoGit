3e776f76aab52c795a6f6411272672d65be460b4
Katerin Martínez <kcmartz01@gmail.com>
2021-08-31 00:23:40 -0600

    Connection agregada

---
+++ config/connection.php
@@ -0,0 +1,11 @@
+<?php
+$host="localhost";
+$user="root";
+$pass="pabykat";
+$db="score_db";
+$conn=new mysqli($host,$user,$pass,$db);
+
+if($conn->connect_errno) {
+    die("La conexion a fallado");
+}
+echo "Conexion con exito.!!";
